53837295f45e33bfe612f1129f4899c011df2016
Brian Chen <enderchen004@outlook.com>
2025-08-10 17:57:47 +0100

    Simple implementation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # LinkedVector-rs
+
 An experimental data structure that might be extremely dumb
+
+## Background
+
+There are two common containers for "dynamic chunks of data of the same type":
+
+- Vectors (dynamic arrays where data are stored in contiguous chunks)
+- Linked Lists (multiple nodes connected via pointers)
+
+One advantage of linked lists is that it supports O(1) deletion - deletion only requires changing one pointer (and freeing the deleted item). For vectors, deletion requires O(n) shifting of all subsequent elements. 
+
+Even with the deletion costs, typically we always prefer vectors simply due to caching - each node of a linked list resides in different chunks in the heap and is likely to result in a cache miss. This cost is often worse than the O(n) cost that comes with shifting elements.
+
+## Experiment
+
+This experiment is a random idea: What happens if we combine the advantages of linked lists and vectors?
+
+We use the same model as linked lists, where each element is accompanied by a pointer to the next item. However, instead of allocating each node on the heap, we allocate them inside a vector (contiguous chunks). We then maintain a stack (using a separate vector) called freelist. Whenever a new item is added, we check the freelist for free spaces - if not, we allocate it inside the new space of the vector. Whenever an item is deleted, we change the pointers just like in a linked list, then add its original index into the freelist.
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,162 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{fmt::Debug, ops::{Index, IndexMut}};
+
+
+struct LinkedNode<T> {
+    item: Option<T>, // Used to facilitate pops
+    next: Option<usize>
 }
+
+struct LinkedVector<T> {
+    data: Vec<LinkedNode<T>>,
+    head: Option<usize>,
+    tail: Option<usize>,
+    freelist: Vec<usize>
+}
+
+impl<T> LinkedVector<T> {
+    fn new() -> LinkedVector<T> {
+        LinkedVector { data: Vec::new(), head: None, tail: None, freelist: Vec::new() }
+    }
+
+    fn alloc(&mut self, new_node: LinkedNode<T>) -> usize {
+        match self.freelist.pop() {
+            Some(idx) => {
+                self.data[idx] = new_node;
+                return idx;
+            },
+            None => {
+                self.data.push(new_node);
+                return self.data.len() - 1;
+            }
+        }
+    }
+
+    pub fn push_front(&mut self, item: T) {
+        let new_node = LinkedNode { item: Some(item), next: self.head };
+        let nidx = self.alloc(new_node);
+
+        self.head = Some(nidx);
+        if self.tail == None { // first element
+            self.tail = Some(nidx);
+        }
+    }
+
+    pub fn head(&self) -> Option<&LinkedNode<T>> {
+        match self.head {
+            Some(idx) => Some(&self.data[idx]),
+            None => None
+        }
+    }
+
+    pub fn tail(&self) -> Option<&LinkedNode<T>> {
+        match self.tail {
+            Some(idx) => Some(&self.data[idx]),
+            None => None
+        }
+    }
+    
+    pub fn tail_mut(&mut self) -> Option<&mut LinkedNode<T>> {
+        match self.tail {
+            Some(idx) => Some(&mut self.data[idx]),
+            None => None
+        }
+    }
+
+    pub fn push_back(&mut self, item: T) {
+        let new_node = LinkedNode { item: Some(item), next: None };
+        let nidx = self.alloc(new_node);
+        match self.tail_mut() {
+            Some(tail_node) => {
+                tail_node.next = Some(nidx);
+                self.tail = Some(nidx);
+            },
+            None => {
+                // New item is both head and tail
+                self.head = Some(nidx);
+                self.tail = Some(nidx);
+            }
+        }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        match self.head {
+            None => None,
+            Some(oidx) => {
+                self.head = self.data[oidx].next;
+                self.freelist.push(oidx);
+                return Some(self.data[oidx].item.take().unwrap());
+            }
+        }
+    }
+    // pop_back not supported - for that, use a doubly implementation
+    //   (or just use delete if you know the length)
+
+
+    pub fn delete(&mut self, idx: usize) -> T {
+        // Will panic if idx out of bounds
+        if idx == 0 {
+            return self.pop_front().unwrap();
+        }
+
+        self.freelist.push(self[idx-1].next.unwrap());
+
+        match self[idx].next {
+            Some(next_idx) => {
+                // idx + 1 exists, needs rerouting
+                self[idx-1].next = Some(next_idx);
+            },
+            None => {
+                // idx + 1 doesn't exist, simply change second-to-last to none
+                self[idx-1].next = None;
+            }
+        }
+        self.data[idx].item.take().unwrap()
+    }
+}
+
+impl<T> Index<usize> for LinkedVector<T> {
+    type Output = LinkedNode<T>;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        let mut current = self.head;
+        for _ in 0..index {
+            current = self.data[current.expect("Index not within bounds")].next;
+        }
+        &self.data[current.expect("Index not within bounds")]
+    }
+}
+
+impl<T> IndexMut<usize> for LinkedVector<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        let mut current = self.head;
+        for _ in 0..index {
+            current = self.data[current.expect("Index not within bounds")].next;
+        }
+        &mut self.data[current.expect("Index not within bounds")]
+    }
+}
+
+
+impl<T: Debug> Debug for LinkedNode<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.item.as_ref().unwrap())
+    }
+}
+
+impl<T: Debug> Debug for LinkedVector<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut idx = self.head;
+        while let Some(idx_num) = idx {
+            idx = self.data[idx_num].next;
+            match write!(f, "{:?} ", self.data[idx_num]) {
+                Ok(_) => (),
+                Err(err) => return Err(err)
+            };
+        }
+        Ok(())
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +164,39 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let mut my_vec = LinkedVector::<u64>::new();
+        assert_eq!(format!("{my_vec:?}").trim(), "");
+
+        my_vec.push_back(100u64);
+        assert_eq!(format!("{my_vec:?}").trim(), "100");
+
+        my_vec.push_back(200u64);
+        assert_eq!(format!("{my_vec:?}").trim(), "100 200");
+
+        my_vec.push_front(300u64);
+        assert_eq!(format!("{my_vec:?}").trim(), "300 100 200");
+        
+        let a = my_vec.pop_front();
+        assert_eq!(format!("{my_vec:?}").trim(), "100 200");
+        assert_eq!(a, Some(300));
+
+        let b = my_vec.delete(1);
+        assert_eq!(format!("{my_vec:?}").trim(), "100");
+        assert_eq!(b, 200);
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_panics() {
+        let mut my_vec = LinkedVector::<u64>::new();
+
+        my_vec.push_back(100u64);
+        my_vec.push_back(200u64);
+        my_vec.push_front(300u64);
+        
+        my_vec.pop_front();
+        my_vec.delete(1);
+        
+        my_vec.delete(1); // Should panic here
     }
 }
